7e0127e0462680499e56a6310a696e9517e8e249
Drew VanDine <avandine23@gmail.com>
2025-04-19 13:25:04 -0400

    Update config.py

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "final_project_api"
     db_port = 3306
     db_user = "root"
-    db_password = "042805dv"
+    db_password = "REPLACE_ME"
     app_host = "localhost"
     app_port = 8000
