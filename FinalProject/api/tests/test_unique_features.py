1aa7daaa68076551f73a51313165adba4c50da20
TareqHuda <thuda@charlotte.edu>
2025-05-02 18:33:36 -0400

    feat: Added code to all test functions but it is buggy.

--- FinalProject/api/tests/test_unique_features.py
+++ FinalProject/api/tests/test_unique_features.py
@@ -1,24 +1,46 @@
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from sqlalchemy.testing import db
+from sqlalchemy.testing.pickleable import Order
 from ..controllers import orders as controller
 from ..main import app
 import pytest
-from ..models import orders as model
+
 
 # Create a test client for the app
 client = TestClient(app)
 
 # Here is the code skeleton.
-def test_calculate_sum_profit_between_days():
-    pass
+def test_calculate_sum_profit_between_days(start_date,end_date):
+    # Create an object.
+    my_order_obj = Order()
+    # Test function by call.
+    #my_order_obj.calculate_sum_profit_between_days(db: Session, order_id, start_date, end_date)
 
-def test_average_time_between_order_statuses():
-    pass
+
+def test_average_time_between_order_statuses(order_id=None):
+    # Create an object.
+    my_order_obj = Order()
+    # Test function by call.
+    #my_order_obj.average_time_between_order_statuses(db: Session, order_id)
+
 
 def test_list_items_by_amount_of_order():
-    pass
+    # Create an object.
+    my_order_obj = Order()
+    # Test function by call.
+    #my_order_obj.list_items_by_amount_of_order(db: Session, order_id)
+
 
 def test_rate_orders():
-    pass
+    # Create an object.
+    my_order_obj = Order()
+    # Test function by call.
+    #my_order_obj.rate_orders(db: Session, order_id)
+
 
 def test_review_orders():
-    pass
+    # Create an object.
+    my_order_obj = Order()
+    # Test function by call.
+    #my_order_obj.review_orders():
